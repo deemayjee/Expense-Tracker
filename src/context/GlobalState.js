83ac5f7f40ee0a4933431f94e030d509ffc90c2c
Dimeji Onatunde <deemayjee@Dimejis-MacBook-Pro.local>
2020-12-22 01:09:44 +0100

    initial commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,40 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+//Initial State
+export const initialState = {
+    transactions: []
+}
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+
+//provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+
+    //Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
+    function addTransaction(transactions) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transactions
+        });
+    }
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>
+        {children}
+    </GlobalContext.Provider>);
+}
